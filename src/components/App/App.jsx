f9c99bef5f10c11964a9c57230a6a03eadb03dd2
antagonist13 <1703shakayaroslaw@gmail.com>
2024-06-07 19:58:07 +0300

    api

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,70 @@
 import css from './App.module.css'
+import { getTrendingMovies, getSarchedMovies, getMovieDetails, getMovieCredits, getMovieReviews } from '../../movies-api'
+import { useState } from 'react';
 
 export default function App() {
-  
+  const [trendMovies, setTrendMovies] = useState();
+  const [searchingMovies, setSearchingMovies] = useState();
+  const [movieDetails, setMovieDetails] = useState();
+  const [movieCredits, setMovieCredits] = useState();
+  const [movieReviews, setMovieReviews] = useState();
+
+    const  handleTrendMovies = async () => {
+    await getTrendingMovies()
+      .then(list => {
+        setTrendMovies(list.data.results)
+      })
+        .finally(() => {
+            console.log(trendMovies);
+          })
+  }
+      const handleSearchMovies = async () => {
+    await getSarchedMovies('batman')
+      .then(list => {
+        setSearchingMovies(list.data.results)
+      })
+      .finally(() => {
+            console.log(searchingMovies);
+          })
+        
+  }
+        const handleMovieDetails = async () => {
+    await getMovieDetails('111')
+      .then(info => {
+        setMovieDetails(info.data)
+      })
+      .finally(() => {
+            console.log(movieDetails);
+          })
+        
+  }
+          const handleMovieCredits = async () => {
+    await getMovieCredits('111')
+      .then(info => {
+        setMovieCredits(info.data)
+      })
+      .finally(() => {
+            console.log(movieCredits);
+          })
+        
+  }
+            const handleMovieReviews = async () => {
+    await getMovieReviews('111')
+      .then(info => {
+        setMovieReviews(info.data.results)
+      })
+      .finally(() => {
+            console.log(movieReviews);
+          })
+        
+  }
+
   return <div className={css.container}>
+    <h1>MOVIES</h1>
+    <button onClick={handleTrendMovies}> Trend Movies</button>
+    <button onClick={handleSearchMovies}> Search Movies</button>
+    <button onClick={handleMovieDetails}> Movie details</button>
+    <button onClick={handleMovieCredits}> Movie credits</button>
+    <button onClick={handleMovieReviews}> Movie reviews</button>
 </div>
 }
--- src/movies-api.js
+++ src/movies-api.js
@@ -1,17 +1,62 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api.unsplash.com";
-const ACESS_KEY = 'd-L6wwOxVoHwVTK9MU_8WHUKXbZoMbaGJ5WvRLBqdN8'
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+const ACESS_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjRiY2QwNTg4YzE4MWE3NTAwMDk3MzhiNjQ2MDBjNyIsInN1YiI6IjY2NjMyNzQ4M2VjNzk2YWI3NjE0ZmM3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6wE9XVMKUlpEUmGCB_RwR2MwQwC1A6qGjx6P57kh1Uc'
 
-export const getImages = async ( query, page) => {
-  const response = await axios.get("/search/photos", {
+export const getTrendingMovies = async ( ) => {
+  const response = await axios.get("/trending/movie/week", {
       params: {
-          per_page: 20,
-          page: page,
-            query: query
+          language: 'en-US',
       },
       headers: {
-            Authorization: `Client-ID ${ACESS_KEY}`,
+            Authorization: `Bearer ${ACESS_KEY}`,
+          },
+  });
+  return response;
+};
+export const getSarchedMovies = async ( query ) => {
+  const response = await axios.get("/search/movie", {
+    params: {
+      query: query,
+      include_adult: false,
+      language: 'en-US',
+      page: 1
+      },
+      headers: {
+            Authorization: `Bearer ${ACESS_KEY}`,
+          },
+  });
+  return response;
+};
+export const getMovieDetails = async ( movieID ) => {
+  const response = await axios.get(`/movie/${movieID}`, {
+    params: {
+      language: 'en-US',
+      },
+      headers: {
+            Authorization: `Bearer ${ACESS_KEY}`,
+          },
+  });
+  return response;
+};
+export const getMovieCredits = async ( movieID ) => {
+  const response = await axios.get(`/movie/${movieID}/credits`, {
+    params: {
+      language: 'en-US',
+      },
+      headers: {
+            Authorization: `Bearer ${ACESS_KEY}`,
+          },
+  });
+  return response;
+};
+export const getMovieReviews = async ( movieID ) => {
+  const response = await axios.get(`/movie/${movieID}/reviews`, {
+    params: {
+      language: 'en-US',
+      },
+      headers: {
+            Authorization: `Bearer ${ACESS_KEY}`,
           },
   });
   return response;
